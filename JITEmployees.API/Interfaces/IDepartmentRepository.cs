61f0984a64dcf351d4038d30a58574bc0ae59964
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-12-15 11:34:39 +0530

    Added IDepartmentsServive

--- JITEmployees.API/Interfaces/IDepartmentRepository.cs
+++ JITEmployees.API/Interfaces/IDepartmentRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace JITEmployees.API.Interfaces
+﻿using JITEmployees.API.Models.Departments;
+
+namespace JITEmployees.API.Interfaces
 {
     public interface IDepartmentRepository
     {
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> CreateAsync(DepartmentsCreateDto dto);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> GetAllAsync(DepartmentsDto dto);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> UpdatedByKeyAsync(DepartmentsUpdateDto dto);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> DeleteByKeyAsync(DepartmentsDeleteDto dto);
     }
 }
