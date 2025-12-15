dbdc610fbf1c6c7648c5196e5533291752682db5
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-12-15 14:07:51 +0530

    Implemented the method of IEmployeesService

---
+++ JITEmployees.API/Services/EmployeesService.cs
@@ -0,0 +1,32 @@
+﻿using JITEmployees.API.Interfaces;
+using JITEmployees.API.Models.Employees;
+
+namespace JITEmployees.API.Services
+{
+    public class EmployeesService : IEmployeesService
+    {
+        private readonly IEmployeesRepository _repo;
+
+        public EmployeesService(IEmployeesRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> CreateAsync(EmployeesCreateDto dto)
+        {
+            return await _repo.CreateAsync(dto);
+        }
+        public Task<(IEnumerable<EmployeesDto> employees, string? ErrorMessage, string? SuccessMessage)> GetAllEmployeesAsync()
+        {
+            return _repo.GetAllAsync();
+        }
+        public async Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> UpdateAsyncById(EmployeesUpdateDto dto)
+        {
+            return await _repo.UpdatedByKeyAsync(dto);
+        }
+        public async Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> DeleteAsyncById(EmployeesDeleteDto dto)
+        {
+            return await _repo.DeleteByKeyAsync(dto);
+        }
+    }
+}
--- JITEmployees.API/obj/Debug/net8.0/JITEmployees.API.AssemblyInfo.cs
+++ JITEmployees.API/obj/Debug/net8.0/JITEmployees.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JITEmployees.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a5c3dd4f2af26a3203ac74ac6be1e7fe8bf4d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d172a7c4c2fd3f3054e3fd01fde36c00524dd94e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JITEmployees.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JITEmployees.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
