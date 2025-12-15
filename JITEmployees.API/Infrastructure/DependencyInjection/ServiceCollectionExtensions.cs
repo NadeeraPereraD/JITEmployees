fc065ba4f5ec62feda4bf2e3602c8c47547c5eba
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-12-15 14:24:57 +0530

    Added Employees Dependency Injection

--- JITEmployees.API/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ JITEmployees.API/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -9,12 +9,14 @@ namespace JITEmployees.API.Infrastructure.DependencyInjection
         public static IServiceCollection AddRepositories(this IServiceCollection repositories)
         {
             repositories.AddScoped<IDepartmentRepository, DepartmentsRepository>();
+            repositories.AddScoped<IEmployeesRepository, EmployeesRepository>();
             return repositories;
         }
 
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
             services.AddScoped<IDepartmentsService, DepartmentsService>();
+            services.AddScoped<IEmployeesService, EmployeesService>();
             return services;
         }
     }
